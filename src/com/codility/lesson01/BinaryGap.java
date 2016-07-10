644bfe9e00c1df296e004856f799345c7774070b
JeongMinCha <cjm9236@naver.com>
2016-07-10 22:17:15 +0900

    01-01: BinaryGap solution

--- src/com/codility/lesson01/BinaryGap.java
+++ src/com/codility/lesson01/BinaryGap.java
@@ -1,10 +1,32 @@
 package com.codility.lesson01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by JeongMinCha on 2016. 7. 10..
  */
 public class BinaryGap {
     public int solution(int N) {
-        return 0;
+        int num = N;
+        List<Boolean> binaryArray = new ArrayList<>();
+
+        while (num != 0) {
+            binaryArray.add(0, (num % 2) != 0);
+            num /= 2;
+        }
+
+        int gap = 0, maxGap = 0;
+        for (Boolean bit : binaryArray) {
+            if (bit.equals(true)) {
+                if (maxGap < gap) {
+                    maxGap = gap;
+                }
+                gap = 0;
+            } else {
+                gap ++;
+            }
+        }
+        return maxGap;
     }
 }
