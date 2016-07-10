91cb91341a0277da195b3eebe08f4848c72e9d84
JeongMinCha <cjm9236@naver.com>
2016-07-10 21:08:26 +0900

    FirstCommit: .gitignore, DriverClass, FirstProblem

---
+++ src/com/codility/lesson01/BinaryGap.java
@@ -0,0 +1,10 @@
+package com.codility.lesson01;
+
+/**
+ * Created by JeongMinCha on 2016. 7. 10..
+ */
+public class BinaryGap {
+    public int solution(int N) {
+        return 0;
+    }
+}
---
+++ src/com/codility/test/DriverClass.java
@@ -0,0 +1,10 @@
+package com.codility.test;
+
+/**
+ * Created by JeongMinCha on 2016. 7. 10..
+ */
+public class DriverClass {
+    public void main(String[] args) {
+
+    }
+}
