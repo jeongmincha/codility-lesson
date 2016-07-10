ec07ab4009c263b3a048fbb61ffae953f6df5b6f
JeongMinCha <cjm9236@naver.com>
2016-07-10 22:57:16 +0900

    02-01: CyclicRotation java file: score 50

--- src/com/codility/lesson02/CyclicRotation.java
+++ src/com/codility/lesson02/CyclicRotation.java
@@ -1,10 +1,27 @@
 package com.codility.lesson02;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by JeongMinCha on 2016. 7. 10..
  */
 public class CyclicRotation {
     public int[] solution(int[] A, int K) {
-        return null;
+        int[] solution = new int[A.length];
+        List<Integer> newList = new ArrayList<Integer>();
+
+        for (int elem : A) {
+            newList.add(elem);
+        }
+        for (int idx = 1; idx <= K; idx++) {
+            newList.add(0, A[A.length - idx]);
+        }
+
+        for (int idx = 0; idx < A.length; idx++) {
+            solution[idx] = newList.get(idx);
+        }
+        return solution;
     }
 }
