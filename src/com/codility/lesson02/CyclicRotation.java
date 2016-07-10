b856333b2be487f3a6d03740612103c438e99c4e
JeongMinCha <cjm9236@naver.com>
2016-07-10 23:08:26 +0900

    02-01: CyclicRotation: processing special cases
    Now, score is 100!

--- src/com/codility/lesson02/CyclicRotation.java
+++ src/com/codility/lesson02/CyclicRotation.java
@@ -9,13 +9,25 @@ import java.util.List;
  */
 public class CyclicRotation {
     public int[] solution(int[] A, int K) {
+        int k = K;
+        if (k == 0 || A.length <= 1) {
+            return A;
+        }
+
+        while (k > A.length) {
+            k -= A.length;
+        }
+        if (k == A.length) {
+            return A;
+        }
+
         int[] solution = new int[A.length];
         List<Integer> newList = new ArrayList<Integer>();
 
         for (int elem : A) {
             newList.add(elem);
         }
-        for (int idx = 1; idx <= K; idx++) {
+        for (int idx = 1; idx <= k; idx++) {
             newList.add(0, A[A.length - idx]);
         }
 
