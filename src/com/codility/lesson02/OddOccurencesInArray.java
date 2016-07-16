4466d4f80901cd97ed48f9026075a8384fa2fcc9
JeongMinCha <cjm9236@naver.com>
2016-07-16 21:10:51 +0900

    02-02: complete odd occurences in array

--- src/com/codility/lesson02/OddOccurencesInArray.java
+++ src/com/codility/lesson02/OddOccurencesInArray.java
@@ -1,5 +1,6 @@
 package com.codility.lesson02;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -7,6 +8,19 @@ import java.util.Map;
  */
 public class OddOccurencesInArray {
     public int solution(int[] A) {
-         return 0;
+        Map<Integer,Integer> counts = new HashMap<>();
+        for (int elem: A) {
+            if (counts.containsKey(elem)) {
+                counts.put(elem, counts.get(elem)+1);
+            } else {
+                counts.put(elem, 1);
+            }
+        }
+        for (int key: counts.keySet()) {
+            if (counts.get(key) % 2 != 0) {
+                return key;
+            }
+        }
+        return 0;
     }
 }
