6f850f4cc0bee5d51010df1e05e4979794d95373
JeongMinCha <cjm9236@naver.com>
2016-07-21 23:16:45 +0900

    04-01: FrogRiverOne Java and JUnit Test Code

---
+++ src/com/codility/lesson04/FrogRiverOne.java
@@ -0,0 +1,10 @@
+package com.codility.lesson04;
+
+/**
+ * Created by JeongMinCha on 2016. 7. 21..
+ */
+public class FrogRiverOne {
+    public int solution(int X, int[] A) {
+        return 0;
+    }
+}
---
+++ src/com/codility/lesson04/FrogRiverOneTest.java
@@ -0,0 +1,27 @@
+package com.codility.lesson04;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by JeongMinCha on 2016. 7. 21..
+ */
+public class FrogRiverOneTest {
+
+    FrogRiverOne frogRiverOne;
+
+    @Before
+    public void setUp() throws Exception {
+        frogRiverOne = new FrogRiverOne();
+    }
+
+    @Test
+    public void solution() throws Exception {
+        int[] arr = {1,3,1,4,2,3,5,4};
+        int answer = 6;
+        assertEquals(answer, frogRiverOne.solution(5, arr));
+    }
+
+}
