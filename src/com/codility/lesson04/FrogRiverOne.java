6b94a9f87dea0725b929a59a6ec9494925f7e35a
JeongMinCha <cjm9236@naver.com>
2016-07-21 23:24:25 +0900

    04-01: Sample Test Case Pass

--- src/com/codility/lesson04/FrogRiverOne.java
+++ src/com/codility/lesson04/FrogRiverOne.java
@@ -1,10 +1,28 @@
 package com.codility.lesson04;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by JeongMinCha on 2016. 7. 21..
  */
 public class FrogRiverOne {
     public int solution(int X, int[] A) {
-        return 0;
+        int idx;
+        int count = 0;
+
+        // K = value, V = index
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (idx = 0; idx < A.length; idx++) {
+            if (!map.containsKey(A[idx])) {
+                map.put(A[idx], ++count);
+            }
+            if (count == X) {
+                break;
+            }
+        }
+
+        return idx;
     }
 }
