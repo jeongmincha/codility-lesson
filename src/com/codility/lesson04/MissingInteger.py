533bee449249701aa09683bd5348b53f5ac3264f
JeongMinCha <cjm9236@naver.com>
2016-07-22 00:25:10 +0900

    04-03: MissingInteger Sample Test Case Pass

--- src/com/codility/lesson04/MissingInteger.py
+++ src/com/codility/lesson04/MissingInteger.py
@@ -1,2 +1,9 @@
 def solution(A):
-    return 0
+    unique_list = sorted(set(A))
+
+    last = 0
+    for e in unique_list:
+        if e is not last+1:
+            return last+1
+        last += 1
+    return -1
