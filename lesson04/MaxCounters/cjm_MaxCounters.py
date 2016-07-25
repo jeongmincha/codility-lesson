6c474261a1eddd9a5345810b6ce1f47c048f7dfd
JeongMinCha <cjm9236@naver.com>
2016-07-26 01:32:06 +0900

    04-04: Correctness Test Pass

--- lesson04/MaxCounters/cjm_MaxCounters.py
+++ lesson04/MaxCounters/cjm_MaxCounters.py
@@ -2,4 +2,13 @@
 Created by JeongMinCha on 2016. 7. 26.
 """
 def solution(N, A):
-    return 0
+    counter = [0] * N
+
+    for e in A:
+        if 1 <= e <= N:
+            counter[e-1] += 1
+        elif e == N+1:
+            mx = max(counter)
+            counter = [mx] * N
+
+    return counter
