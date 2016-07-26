c8ac704c3819debd7838002bb3f396306b73d9eb
JeongMinCha <cjm9236@naver.com>
2016-07-26 13:50:35 +0900

    05-02: CountDiv 100% Test Pass

--- lesson05/CountDiv/JeongMinCha_CountDiv.py
+++ lesson05/CountDiv/JeongMinCha_CountDiv.py
@@ -2,8 +2,7 @@
 Created by JeongMinCha on 2016. 7. 26.
 """
 def solution(A, B, K):
-    ret = 0
-    for elem in range(A, B+1):
-        if elem % K == 0:
-            ret += 1
-    return ret
+    if A%K == 0:
+        return (B-A)/K + 1
+    else:
+        return (B-(A-A%K))/K
