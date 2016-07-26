82e8ddb062468a8a7747e99ba52b1251304a8087
JeongMinCha <cjm9236@naver.com>
2016-07-26 14:23:59 +0900

    05-03: GenomicRangeQuery correctness pass

--- lesson05/GenomicRangeQuery/JeongMinCha_GenomicRangeQuery.py
+++ lesson05/GenomicRangeQuery/JeongMinCha_GenomicRangeQuery.py
@@ -2,4 +2,11 @@
 Created by JeongMinCha on 2016. 7. 26.
 """
 def solution(S, P, Q):
-    return 0
+    minimal_factors = []
+    impact = dict({'A':1, 'C':2, 'G': 3, 'T': 4})
+
+    for idx in range(len(P)):
+        mn = min([impact[S[idx]] for idx in range(P[idx], Q[idx]+1)])
+        minimal_factors.append(mn)
+
+    return minimal_factors
