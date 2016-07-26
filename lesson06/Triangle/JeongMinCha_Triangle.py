9e75ca09d9bb7d676cf3a3866d5d6730d7f035b5
JeongMinCha <cjm9236@naver.com>
2016-07-27 00:53:38 +0900

    06-03: Triangle 100% Pass

--- lesson06/Triangle/JeongMinCha_Triangle.py
+++ lesson06/Triangle/JeongMinCha_Triangle.py
@@ -2,4 +2,13 @@
 Created by JeongMinCha on 2016. 7. 27.
 """
 def solution(A):
+    if len(A) < 3:
+        return 0
+    A.sort()
+    if A[-1] < 0:
+        return 0
+
+    for idx in range(0, len(A)-2):
+        if A[idx] + A[idx+1] > A[idx+2]:
+            return 1
     return 0
