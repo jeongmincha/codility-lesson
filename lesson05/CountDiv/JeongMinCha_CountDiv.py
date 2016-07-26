ab05706b4fb257811b687c492be5ec24390041f4
JeongMinCha <cjm9236@naver.com>
2016-07-26 11:47:59 +0900

    05-02: CountDiv Correctness Test Pass

--- lesson05/CountDiv/JeongMinCha_CountDiv.py
+++ lesson05/CountDiv/JeongMinCha_CountDiv.py
@@ -2,4 +2,8 @@
 Created by JeongMinCha on 2016. 7. 26.
 """
 def solution(A, B, K):
-    return 0
+    ret = 0
+    for elem in range(A, B+1):
+        if elem % K == 0:
+            ret += 1
+    return ret
