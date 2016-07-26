e9402e893ea4f2e0fb7a20b729d67797daeff6f4
JeongMinCha <cjm9236@naver.com>
2016-07-26 11:36:59 +0900

    05-01: PassingCars 100% Pass

--- lesson05/PassingCars/JeongMinCha_PassingCars.py
+++ lesson05/PassingCars/JeongMinCha_PassingCars.py
@@ -2,4 +2,14 @@
 Created by JeongMinCha on 2016. 7. 21.
 """
 def solution(A):
-    return 0
+    MAXIMUM = 1000000000
+    passing = 0
+    east = 0
+    for elem in A:
+        if elem == 0:
+            east += 1
+        else:
+            passing += east
+            if passing > MAXIMUM:
+                return -1
+    return passing
