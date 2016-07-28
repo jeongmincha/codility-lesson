59bc308bbbb005d4b57619e77e142a65ba12e442
JeongMinCha <cjm9236@naver.com>
2016-07-29 01:43:15 +0900

    08-02: Dominator python and unit test code

---
+++ lesson08/Dominator/JeongMinCha_DominatorTest.py
@@ -0,0 +1,14 @@
+"""
+Created by JeongMinCha on 2016. 7. 29.
+"""
+import unittest
+import JeongMinCha_Dominator as Dominator
+
+class TestDominator(unittest.TestCase):
+    def test_solution(self):
+        A = [3, 4, 3, 2, 3, -1, 3, 3]
+        expected = [0, 2, 4, 6, 7]
+        self.assertEqual(expected, Dominator.solution(A))
+
+if __name__ == '__main__':
+    unittest.main()
