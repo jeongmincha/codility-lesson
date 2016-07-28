b7135898bd0bbc0ddbc65dc3b6ebf0a2f1aeae00
JeongMinCha <cjm9236@naver.com>
2016-07-29 00:39:29 +0900

    07-03: Nesting 100% Test Pass

--- lesson07/Nesting/JeongMinCha_Nesting.py
+++ lesson07/Nesting/JeongMinCha_Nesting.py
@@ -2,4 +2,16 @@
 Created by JeongMinCha on 2016. 7. 29.
 """
 def solution(S):
-    return 0
+    stack = []
+    for s in S:
+        if s == '(':
+            stack.append(s)
+        else:
+            if len(stack) == 0:
+                return 0
+            else:
+                stack.pop()
+    if len(stack) == 0:
+        return 1
+    else:
+        return 0
