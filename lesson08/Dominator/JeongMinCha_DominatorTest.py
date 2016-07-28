de62e4d52a6351f2aed6a0e7e2ecc4f64197f144
JeongMinCha <cjm9236@naver.com>
2016-07-29 01:51:04 +0900

    08-02: Dominator modify return type from list to integer

--- lesson08/Dominator/JeongMinCha_DominatorTest.py
+++ lesson08/Dominator/JeongMinCha_DominatorTest.py
@@ -8,7 +8,7 @@ class TestDominator(unittest.TestCase):
     def test_solution(self):
         A = [3, 4, 3, 2, 3, -1, 3, 3]
         expected = [0, 2, 4, 6, 7]
-        self.assertEqual(expected, Dominator.solution(A))
+        assert Dominator.solution(A) in expected
 
 if __name__ == '__main__':
     unittest.main()
