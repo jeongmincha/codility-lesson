bba2c19bbd65c37a97af612c45a47ac475b2a583
JeongMinCha <cjm9236@naver.com>
2016-07-29 01:38:06 +0900

    08-01: EquiLeader 100% Test Pass

--- lesson08/EquiLeader/JeongMinCha_EquiLeader.py
+++ lesson08/EquiLeader/JeongMinCha_EquiLeader.py
@@ -2,4 +2,32 @@
 Created by JeongMinCha on 2016. 7. 29.
 """
 def solution(A):
-    return 0
+    candidate = -1
+    candidate_cnt = 0
+
+    for idx in range(0, len(A)):
+        if candidate_cnt == 0:
+            candidate = A[idx]
+            candidate_cnt += 1
+        else:
+            if A[idx] == candidate:
+                candidate_cnt += 1
+            else:
+                candidate_cnt -= 1
+
+    leader_count = len([number for number in A if number == candidate])
+    if leader_count <= len(A) / 2:
+        return 0
+    else:
+        leader = candidate
+
+    equi_leaders = 0
+    front_leader_cnt = 0
+    for idx in range(0, len(A)):
+        if A[idx] == leader:
+            front_leader_cnt += 1
+        if front_leader_cnt > (idx+1) / 2 and \
+           leader_count-front_leader_cnt > (len(A) - (idx+1)) / 2:
+            equi_leaders += 1
+
+    return equi_leaders
