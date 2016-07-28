c8aa14387be91ded2bf61c0e40537f8ee311414e
JeongMinCha <cjm9236@naver.com>
2016-07-29 01:03:22 +0900

    07-04: StoneWall 100% Test Pass

--- lesson07/StoneWall/JeongMinCha_StoneWall.py
+++ lesson07/StoneWall/JeongMinCha_StoneWall.py
@@ -2,4 +2,16 @@
 Created by JeongMinCha on 2016. 7. 29.
 """
 def solution(H):
-    return 0
+    stones = 0
+    stack = []
+
+    for h in H:
+        while len(stack) > 0 and h < stack[-1]:
+            stack.pop()
+        if len(stack) > 0 and h == stack[-1]:
+            continue
+        else:
+            stones += 1
+            stack.append(h)
+
+    return stones
