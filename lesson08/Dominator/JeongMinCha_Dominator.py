9fa874a4aea2f6f054ec6f97fdd77d9fc22544ae
JeongMinCha <cjm9236@naver.com>
2016-07-29 01:52:31 +0900

    08-02: Dominator 100% Test Pass

--- lesson08/Dominator/JeongMinCha_Dominator.py
+++ lesson08/Dominator/JeongMinCha_Dominator.py
@@ -2,4 +2,19 @@
 Created by JeongMinCha on 2016. 7. 29.
 """
 def solution(A):
-    return 0
+    cnt_dict = {}
+    idx_dict = {}
+
+    for (idx, a) in enumerate(A):
+        if a not in cnt_dict:
+            cnt_dict[a] = 1
+            idx_dict[a] = [idx]
+        else:
+            cnt_dict[a] += 1
+            idx_dict[a].append(idx)
+
+    for val in cnt_dict.keys():
+        if cnt_dict[val] > len(A) / 2:
+            return idx_dict[val][0]
+
+    return -1
