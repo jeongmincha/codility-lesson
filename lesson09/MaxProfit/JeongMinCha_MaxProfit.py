70acb4495280636aa081b26adbca562a1987133c
JeongMinCha <cjm9236@naver.com>
2016-07-30 00:39:16 +0900

    09-02: MaxProfit python and unit test code

--- lesson09/MaxProfit/JeongMinCha_MaxProfit.py
+++ lesson09/MaxProfit/JeongMinCha_MaxProfit.py
@@ -2,4 +2,11 @@
 Created by JeongMinCha on 2016. 7. 30.
 """
 def solution(A):
-    return 0
+    max_so_far = A[-1]
+    max_profit = 0
+
+    for idx in range(len(A)-1, 0, -1):
+        max_profit = max(max_profit, max_so_far-A[idx])
+        max_so_far = max(A[idx], max_so_far)
+
+    return max_profit
