41c3f8bb40d663e44d404573f59e196f2a2683de
JeongMinCha <cjm9236@naver.com>
2016-07-30 00:19:28 +0900

    09-01: MaxDoubleSliceSum 100% Test Pass

--- lesson09/MaxDoubleSliceSum/JeongMinCha_MaxDoubleSliceSum.py
+++ lesson09/MaxDoubleSliceSum/JeongMinCha_MaxDoubleSliceSum.py
@@ -1,6 +1,22 @@
 """
-Created by JeongMinCha on 2016. 7. 29.
+Created by JeongMinCha on 2016. 7. 30.
 """
 def solution(A):
-    return 0
+    max_ending_here = [0] * len(A)
+    max_starting_here = [0] * len(A)
 
+    max_so_far1 = 0
+    for idx in range(1, len(A)-1):
+        max_so_far1 = max(0, A[idx] + max_so_far1)
+        max_ending_here[idx] = max_so_far1
+
+    max_so_far2 = 0
+    for idx in range(len(A)-2, 0, -1):
+        max_so_far2 = max(0, A[idx] + max_so_far2)
+        max_starting_here[idx] = max_so_far2
+
+    max_double_slice = 0
+    for idx in range(0, len(A)-2):
+        max_double_slice = max(max_double_slice, max_ending_here[idx] + max_starting_here[idx+2])
+
+    return max_double_slice
