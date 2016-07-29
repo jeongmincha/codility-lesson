fec08916f3c389f4bdebeee97ab8b1dd1c0a36cc
JeongMinCha <cjm9236@naver.com>
2016-07-30 00:45:15 +0900

    09-02: add guarding & modify indices to access

--- lesson09/MaxProfit/JeongMinCha_MaxProfit.py
+++ lesson09/MaxProfit/JeongMinCha_MaxProfit.py
@@ -2,10 +2,13 @@
 Created by JeongMinCha on 2016. 7. 30.
 """
 def solution(A):
+    if len(A) < 2:
+        return 0
+
     max_so_far = A[-1]
     max_profit = 0
 
-    for idx in range(len(A)-1, 0, -1):
+    for idx in range(len(A)-2, -1, -1):
         max_profit = max(max_profit, max_so_far-A[idx])
         max_so_far = max(A[idx], max_so_far)
 
