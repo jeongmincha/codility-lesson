b55709bb2bf56cc1f022f50fb4a898df2c11e6e0
JeongMinCha <cjm9236@naver.com>
2016-07-30 01:22:51 +0900

    09-03: MaxSliceSum 100% Test Pass

--- lesson09/MaxSliceSum/JeongMinCha_MaxSliceSum.py
+++ lesson09/MaxSliceSum/JeongMinCha_MaxSliceSum.py
@@ -2,4 +2,10 @@
 Created by JeongMinCha on 2016. 7. 30.
 """
 def solution(A):
-    return 0
+    max_so_far = max_ending_here = A[0]
+
+    for idx in range(1, len(A)):
+        max_ending_here = max(A[idx], max_ending_here + A[idx])
+        max_so_far = max(max_so_far, max_ending_here)
+
+    return max_so_far
--- lesson09/MaxSliceSum/JeongMinCha_MaxSliceSumTest.py
+++ lesson09/MaxSliceSum/JeongMinCha_MaxSliceSumTest.py
@@ -12,3 +12,4 @@ class TestMaxSliceSum(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
