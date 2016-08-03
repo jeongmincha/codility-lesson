6b4579bb36e94b1c2db84a478cef2d9b500f6f91
JeongMinCha <cjm9236@naver.com>
2016-08-03 15:59:54 +0900

    10-02: CountFactors 100% Test Pass

--- lesson10/CountFactors/JeongMinCha_CountFactors.py
+++ lesson10/CountFactors/JeongMinCha_CountFactors.py
@@ -2,4 +2,33 @@
 Created by JeongMinCha on 2016. 8. 3.
 """
 def solution(N):
-    return 0
+    factors = primes(N)
+    # key = factor, value = number of the factor
+    fact_map = {}
+
+    for factor in factors:
+        if fact_map.has_key(factor):
+            fact_map[factor] += 1
+        else:
+            fact_map[factor] = 1
+
+    # Multipliy all of (number of factors + 1)
+    num_factors = 1
+    for factor in fact_map.keys():
+        num_factors *= (fact_map[factor] + 1)
+
+    return num_factors
+
+# Prime Factorization
+# e.g. 24 => [2,2,2,3]
+def primes(N):
+    factors = []
+    d = 2
+    while d*d <= N:
+        while N % d == 0:
+            factors.append(d)
+            N //= d
+        d += 1
+    if N > 1:
+        factors.append(N)
+    return factors
