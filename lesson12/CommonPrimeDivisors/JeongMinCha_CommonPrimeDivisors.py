dfb11f2dc1a0c2cd3474738f351513e027be5831
JeongMinCha <better.jeongmin@gmail.com>
2016-08-04 23:52:15 +0900

    12-02: using prime factorization -> 100% / 66%
    The code is 100% for correctness test, but 66% for peformance test

--- lesson12/CommonPrimeDivisors/JeongMinCha_CommonPrimeDivisors.py
+++ lesson12/CommonPrimeDivisors/JeongMinCha_CommonPrimeDivisors.py
@@ -2,4 +2,22 @@
 Created by JeongMinCha on 2016. 8. 4.
 """
 def solution(A, B):
-    return 0
+    pairs = 0
+    for idx in range(len(A)):
+        factorA = set(prime_factorization(A[idx]))
+        factorB = set(prime_factorization(B[idx]))
+        if factorA.issubset(factorB) and factorA.issuperset(factorB):
+            pairs +=1
+    return pairs
+
+def prime_factorization(N):
+    factors = []
+    p = 2
+    while p * p <= N:
+        while N % p == 0:
+            factors.append(p)
+            N //= p
+        p += 1
+    if N > 1:
+        factors.append(N)
+    return factors
