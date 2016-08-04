af37c10442a22e85b5bd2816ed00ede480e22943
JeongMinCha <better.jeongmin@gmail.com>
2016-08-04 23:31:11 +0900

    12-01: Correctness Test Pass

--- lesson12/ChocolatesByNumbers/JeongMinCha_ChocolatesByNumbers.py
+++ lesson12/ChocolatesByNumbers/JeongMinCha_ChocolatesByNumbers.py
@@ -1,5 +1,16 @@
 """
 Created by JeongMinCha on 2016. 8. 4.
 """
-def solution(M, N):
-    return 0
+def solution(N, M):
+    chocolates = [True] * N
+    chocolates[0] = False
+
+    num_eating = 1
+    pos = M
+
+    while chocolates[pos%N] == True:
+        chocolates[pos%N] = False
+        pos += M
+        num_eating += 1
+
+    return num_eating
