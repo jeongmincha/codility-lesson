2670170b398de70e5ef3183fae015e09ed7c5c0e
munchkin99 <munchkin99@naver.com>
2016-08-05 22:49:53 +0900

    NYKim

---
+++ lesson10/CountFactors/NayoungKim_CountFactors.py
@@ -0,0 +1,11 @@
+﻿def solution(N):
+    i = 1
+    num = 0
+    while i * i < N:
+        if N % i == 0:
+            num += 2
+        i += 1
+    if i * i == N:
+        num += 1
+        
+    return num
---
+++ lesson11/CountSemiprimes/NayoungKim_CountSemiprimes.py
@@ -0,0 +1,25 @@
+﻿def solution(N, P, Q):
+    n = N
+    l = Q[0] - P[0]
+    F = [0] * (n+1)
+    i = 2
+    result = [0]*len(P)
+    temp = 0
+    while i * i <= n:
+        if F[i] == 0:
+            k = i * i
+            while k <= n:
+                if F[k] == 0:
+                    F[k] = i
+                k += i
+        i += 1
+    
+    for a in range(0,len(P)):
+        for j in range(2,n):
+            for k in range(j,n):
+                if F[j] == 0 and F[k]==0:
+                    temp = j * k
+                    if temp >= P[a] and temp <= Q[a]:
+                        result[a] += 1
+                    
+    return result
