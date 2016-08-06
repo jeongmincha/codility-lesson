1d5026fa87eea546447cf1e328638154946d00fb
munchkin99 <munchkin99@naver.com>
2016-08-06 13:56:10 +0900

    Ch11 & 12 Modified

--- lesson11/CountSemiprimes/NayoungKim_CountSemiprimes.py
+++ lesson11/CountSemiprimes/NayoungKim_CountSemiprimes.py
@@ -1,25 +1,41 @@
-﻿def solution(N, P, Q):
-    n = N
-    l = Q[0] - P[0]
-    F = [0] * (n+1)
+﻿def sieve(n):
+    sieve = [True] * (n + 1)
+    semi = set()
+    sieve[0] = sieve[1] = False
     i = 2
-    result = [0]*len(P)
-    temp = 0
     while i * i <= n:
-        if F[i] == 0:
+        if sieve[i]:
             k = i * i
             while k <= n:
-                if F[k] == 0:
-                    F[k] = i
+                sieve[k] = False
                 k += i
         i += 1
+    i = 2
+    while i * i <= n:
+        if sieve[i] == True:
+            for j in xrange(i*i, n+1, i):
+                if sieve[j/i]==True:
+                    semi.add(j)
+        i += 1
+            
+    return semi
+
+def solution(N, P, Q):
+    semi = sieve(N)
+    count = []
+    count.append(0) # 0
+    count.append(0) # 1
+    count.append(0) # 2
+    count.append(0) # 3
+    count.append(1) # 4
     
-    for a in range(0,len(P)):
-        for j in range(2,n):
-            for k in range(j,n):
-                if F[j] == 0 and F[k]==0:
-                    temp = j * k
-                    if temp >= P[a] and temp <= Q[a]:
-                        result[a] += 1
-                    
+    for i in xrange(5, max(Q)+1):
+        if i in semi:
+            count.append(count[-1]+1)
+        else:
+            count.append(count[-1])
+    
+    result = []
+    for i in xrange(len(Q)):
+        result.append(count[Q[i]] - count[P[i]-1])
     return result
