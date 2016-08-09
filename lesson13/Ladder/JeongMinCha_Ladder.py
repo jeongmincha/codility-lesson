6740f9f35c985b293de6167084f8944e4cb2c3b9
JeongMinCha <better.jeongmin@gmail.com>
2016-08-10 00:38:45 +0900

    13-02: 100% Pass /using & instead of %

--- lesson13/Ladder/JeongMinCha_Ladder.py
+++ lesson13/Ladder/JeongMinCha_Ladder.py
@@ -4,18 +4,20 @@ Created by JeongMinCha on 2016. 8. 9.
 
 
 def solution(A, B):
-    res = list()
-    fibonacci_list = fibonacci(max(A)+1)
+    res = [0] * len(A)
+    max_mod = (1 << max(B)) - 1
+    fib = fibonacci(max(A), max_mod)
+
     for idx in xrange(len(A)):
         a = A[idx]
         b = B[idx]
-        res.append(fibonacci_list[a+1] % pow(2, b))
+        res[idx] = fib[a+1] & ((1 << b) - 1)
     return res
 
 
-def fibonacci(N):
+def fibonacci(N, max_mod):
     fib = [0] * (N+2)
     fib[1] = 1
-    for i in xrange(2, N+1):
-        fib[i] = fib[i-1] + fib[i-2]
+    for i in xrange(2, N+2):
+        fib[i] = (fib[i-1] + fib[i-2]) & max_mod
     return fib
