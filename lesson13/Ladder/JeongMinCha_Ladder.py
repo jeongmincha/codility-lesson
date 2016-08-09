cced50c10561aafec5fdc04fd8f91b86929d683b
JeongMinCha <better.jeongmin@gmail.com>
2016-08-09 23:58:46 +0900

    13-02: Ladder first solution by using fibonacci

--- lesson13/Ladder/JeongMinCha_Ladder.py
+++ lesson13/Ladder/JeongMinCha_Ladder.py
@@ -4,4 +4,17 @@ Created by JeongMinCha on 2016. 8. 9.
 
 
 def solution(A, B):
-    return 0
+    res = list()
+    for idx in xrange(len(A)):
+        a = A[idx]
+        b = B[idx]
+        res.append(fibonacci(a+1) % pow(2, b))
+    return res
+
+
+def fibonacci(N):
+    fib = [0] * (N+2)
+    fib[1] = 1
+    for i in xrange(2, N+1):
+        fib[i] = fib[i-1] + fib[i-2]
+    return fib[N]
