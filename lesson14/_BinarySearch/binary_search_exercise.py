a5f1fd1d51731833c33ad0dfcfd4f06ce0896b5f
JeongMinCha <better.jeongmin@gmail.com>
2016-08-10 01:02:08 +0900

    details of exercise using binary search

---
+++ lesson14/_BinarySearch/binary_search_exercise.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created by JeongMinCha on 2016. 8. 10
+
+Problem: You are given n binary values x0, x1, . . . , xn−1,
+such that xi ∈ {0, 1}. This array represents holes in a roof
+(1 is a hole). You are also given k boards of the same size.
+The goal is to choose the optimal (minimal) size of the boards
+that allows all the holes to be covered by boards..
+"""
