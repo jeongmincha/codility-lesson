209d49edfd09bc1783f5cc10896b60479ebb5a54
JeongMinCha <better.jeongmin@gmail.com>
2016-08-10 00:26:11 +0900

    13-02: 100% correctness, 75% performance

--- lesson13/Ladder/JeongMinCha_Ladder.py
+++ lesson13/Ladder/JeongMinCha_Ladder.py
@@ -5,10 +5,11 @@ Created by JeongMinCha on 2016. 8. 9.
 
 def solution(A, B):
     res = list()
+    fibonacci_list = fibonacci(max(A)+1)
     for idx in xrange(len(A)):
         a = A[idx]
         b = B[idx]
-        res.append(fibonacci(a+1) % pow(2, b))
+        res.append(fibonacci_list[a+1] % pow(2, b))
     return res
 
 
@@ -17,4 +18,4 @@ def fibonacci(N):
     fib[1] = 1
     for i in xrange(2, N+1):
         fib[i] = fib[i-1] + fib[i-2]
-    return fib[N]
+    return fib
