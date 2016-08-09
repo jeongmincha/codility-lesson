2351a17dccb79ae4db1f9c638dc48003c5cc6ac2
JeongMinCha <better.jeongmin@gmail.com>
2016-08-10 01:45:01 +0900

    14-01: binary search for max block sum

--- lesson14/MinMaxDivision/JeongMinCha_MinMaxDivision.py
+++ lesson14/MinMaxDivision/JeongMinCha_MinMaxDivision.py
@@ -4,4 +4,35 @@ Created by JeongMinCha on 2016. 8. 10.
 
 
 def solution(K, M, A):
-    return 0
+    beg = max(A)
+    end = sum(A)
+
+    if K == 1:
+        return end
+    if K >= len(A):
+        return beg
+
+    while beg <= end:
+        mid = (beg + end) / 2
+        if block_size_is_valid(A, K, mid):
+            end = mid - 1
+        else:
+            beg = mid + 1
+
+    return beg
+
+
+def block_size_is_valid(A, max_block_num, max_block_sum):
+    block_sum = 0
+    block_cnt = 0
+
+    last = -1
+    for element in A:
+        if block_sum + element > max_block_sum:
+            block_sum = element
+            block_cnt += 1
+        else:
+            block_sum += element
+        if block_cnt >= max_block_num:
+            return False
+    return True
