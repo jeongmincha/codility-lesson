453fc740d6a0515e4b58b10034cc00adc12f9d2a
JeongMinCha <better.jeongmin@gmail.com>
2016-08-12 21:33:02 +0900

    15-01 AbsDistinct solution using caterpillar method

--- lesson15/AbsDistinct/JeongMinCha_AbsDistinct.py
+++ lesson15/AbsDistinct/JeongMinCha_AbsDistinct.py
@@ -2,5 +2,37 @@
 Created by JeongMinCha on 2016. 8. 12.
 """
 
+
 def solution(A):
-    return 0
+    """
+    AbsDistinct solution by JeongMinCha
+    This method uses the caterpillar method
+    :param A: input array
+    :return: how many distinct absolute values exist
+    """
+    beg = 0
+    end = len(A) - 1
+    abs_distinct = 1
+    current = max(abs(A[0]), abs(A[-1]))
+
+    while beg <= end:
+        former = abs(A[beg])
+        if former == current:
+            beg += 1
+            continue
+
+        latter = abs(A[end])
+        if latter == current:
+            end -= 1
+            continue
+
+        if former >= latter:
+            current = former
+            beg += 1
+        else:
+            current = latter
+            end -= 1
+
+        abs_distinct += 1
+
+    return abs_distinct
