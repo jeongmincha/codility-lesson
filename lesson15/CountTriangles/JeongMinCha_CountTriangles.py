5892544b8dcba7c39fb382b658c7b1b149c858e2
JeongMinCha <better.jeongmin@gmail.com>
2016-08-12 22:50:42 +0900

    15-03 CountTriangles solution O(n2)

--- lesson15/CountTriangles/JeongMinCha_CountTriangles.py
+++ lesson15/CountTriangles/JeongMinCha_CountTriangles.py
@@ -4,4 +4,15 @@ Created by JeongMinCha on 2016. 8. 12.
 
 
 def solution(A):
-    return 0
+    num_triangle = 0
+    N = len(A)
+    sA = sorted(A)
+
+    for x in xrange(N):
+        z = x + 2
+        for y in xrange(x+1, N):
+            while z < N and sA[x] + sA[y] > sA[z]:
+                z += 1
+            num_triangle += z - y - 1
+
+    return num_triangle
