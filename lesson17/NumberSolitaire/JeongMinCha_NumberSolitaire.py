334e98a70e527c07d61ccf86c3acade493d4c9cd
JeongMinCha <better.jeongmin@gmail.com>
2016-08-16 01:13:42 +0900

    NumberSolitaire 100% Test Pass time-O(N), space-O(N)

--- lesson17/NumberSolitaire/JeongMinCha_NumberSolitaire.py
+++ lesson17/NumberSolitaire/JeongMinCha_NumberSolitaire.py
@@ -4,4 +4,10 @@ Created by JeongMinCha on 2016. 8. 16.
 
 
 def solution(A):
-    return 0
+    N = len(A)
+    dp = [A[0]] * (N+6)
+
+    for idx in xrange(1, N):
+        dp[idx+6] = max(dp[idx:idx+6]) + A[idx]
+
+    return dp[-1]
