7a97467df098302365f0ca1e59d8e339fa16638c
JeongMinCha <better.jeongmin@gmail.com>
2016-08-15 23:09:03 +0900

    TieRopes 100% Test Pass - O(n)

--- lesson16/TieRopes/JeongMinCha_TieRopes.py
+++ lesson16/TieRopes/JeongMinCha_TieRopes.py
@@ -4,4 +4,12 @@ Created by JeongMinCha on 2016. 8. 15.
 
 
 def solution(K, A):
-    return 0
+    ropes = 0
+    cur_length = 0
+    for idx in xrange(len(A)):
+        cur_length += A[idx]
+        if cur_length >= K:
+            cur_length = 0
+            ropes += 1
+
+    return ropes
