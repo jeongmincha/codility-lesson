bb1fd53b537d28975a8c28257d36355f4f071adb
JeongMinCha <better.jeongmin@gmail.com>
2016-08-15 21:36:17 +0900

    15-04 100% Test Pass using Caterpillar method

--- lesson15/MinAbsSumOfTwo/JeongMinCha_MinAbsSumOfTwo.py
+++ lesson15/MinAbsSumOfTwo/JeongMinCha_MinAbsSumOfTwo.py
@@ -4,4 +4,26 @@ Created by JeongMinCha on 2016. 8. 15.
 
 
 def solution(A):
-    return 0
+    A.sort()
+
+    if A[0] >= 0:
+        return 2 * A[0]
+    if A[-1] <= 0:
+        return -2 * A[-1]
+
+    front, back = len(A)-1, 0
+    minAbs = 2 * A[-1]
+
+    while back <= front:
+        cur = abs(A[back] + A[front])
+        if minAbs > cur:
+            minAbs = cur
+
+        if abs(A[back+1] + A[front]) <= cur:
+            back += 1
+        elif abs(A[back] + A[front-1]) <= cur:
+            front -= 1
+        else:
+            back += 1; front -= 1
+
+    return minAbs
